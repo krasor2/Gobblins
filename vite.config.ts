6a5b08f8f8aa10863666da28d058bf83ce3ae359
krasor2 <krasor2@gmail.com>
2026-07-27 11:18:07 +0200

    chore: configure single-file Vite build

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vite';
+import { viteSingleFile } from 'vite-plugin-singlefile';
+
+export default defineConfig({
+  base: './',
+  plugins: [viteSingleFile()],
+  build: {
+    target: 'es2022',
+    cssCodeSplit: false,
+    assetsInlineLimit: 100_000_000,
+    sourcemap: false,
+  },
+});
